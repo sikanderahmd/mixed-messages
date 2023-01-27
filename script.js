fac4e2ec076af2b94912e12ec79436dd390459d0
Sikander Ahmed <98826085+skndrahmd@users.noreply.github.com>
2023-01-27 16:08:32 +0500

    Final commit

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("Hello world")
-const quotes = ['Trust the process', 'You will, You can, you must']
+
+const quotes = {
+arr1 : ['Trust the process', 'You will, You can, you must', 'Be okay with failure'], 
+arr2 : ['Failure helps you grow', 'Take the pain like a champ!', 'There is always ease after hardship'], 
+arr3 : ['Keep going!', 'It is not a sprint, it is a marathon!', 'Go get it!', 'Seize the day!']
+}
+
+const word_1 = quotes.arr1[(Math.floor(Math.random() * quotes.arr1.length))]
+const word_2 = quotes.arr2[(Math.floor(Math.random() * quotes.arr2.length))]
+const word_3 = quotes.arr3[(Math.floor(Math.random() * quotes.arr3.length))]
+console.log('Motivation for you!')
+console.log(word_1 + ', ' + word_2 + ', ' + word_3)
